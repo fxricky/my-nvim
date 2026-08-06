bf81a25d1a2049ffd4069312765b088b0ec63e40
fxricky <fanxuricky@gmail.com>
2026-08-06 09:05:48 +0800

    add golsp, ignore node_modules in fzf

--- lua/plugins/snacks-picker.lua
+++ lua/plugins/snacks-picker.lua
@@ -7,10 +7,17 @@ return {
           files = {
             hidden = true,
             ignored = true,
+            exclude = { "node_modules" },
+          },
+          grep = {
+            hidden = true,
+            ignored = true,
+            exclude = { "node_modules" },
           },
           explorer = {
             hidden = true,
             ignored = true,
+            exclude = { "node_modules" },
           },
         },
       },
